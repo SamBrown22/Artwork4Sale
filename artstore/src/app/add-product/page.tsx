5280e4eecd1e5194bc7e7a0fe2ceb2575160fbd0
sb989 <sb989@student.le.ac.uk>
2024-10-28 23:00:28 +0000

    - Moved the server logic to a services directory for better organisation

--- artstore/src/app/add-product/page.tsx
+++ artstore/src/app/add-product/page.tsx
@@ -1,32 +1,7 @@
-import { prisma } from "@/utils/db/prisma";
-import { redirect } from "next/navigation";
+import { addProduct } from "@/services/productService";
 
 export const metadata = {
   title: "Add Product - Artwork4Sale",
-}
-
-async function addProduct(formData: FormData) {
-  "use server";
-
-  const name = formData.get("name")?.toString();
-  const description = formData.get("description")?.toString();
-  const imageUrl = formData.get("imageUrl")?.toString();
-  const price = Number(formData.get("price") || 0);
-
-  if (!name || !description || !imageUrl || !price) {
-    throw Error("Please fill out all fields.");
-  }  
-  
-  await prisma.product.create({
-    data: { 
-      name: name,
-      description: description,
-      imageUrl: imageUrl,
-      price: price,
-    },
-  });
-
-  redirect('/');
 }
 
 export default function AddProductPage() {
---
+++ artstore/src/services/productService.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { prisma } from "@/utils/db/prisma";
+import { redirect } from "next/navigation";
+
+export async function addProduct(formData : FormData) {
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Please fill out all fields.");
+  }  
+  
+  await prisma.product.create({
+    data: { 
+      name: name,
+      description: description,
+      imageUrl: imageUrl,
+      price: price,
+    },
+  });
+
+  redirect('/');
+}
