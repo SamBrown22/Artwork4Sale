d952706bb617bb2ad7ad01751ee2f773fad65b82
sb989 <sb989@student.le.ac.uk>
2024-10-28 16:04:20 +0000

    - Continued Setup for webpage including tailwind, prisma, daisyUI

--- artstore/tailwind.config.ts
+++ artstore/tailwind.config.ts
@@ -1,18 +1,28 @@
 import type { Config } from "tailwindcss";
+import daisyui from 'daisyui';
 
 const config: Config = {
   content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {        
+          primary: "#7e00ff",      
+          secondary: "#00f200",     
+          accent: "#bf8200",
+          neutral: "#090a00",                    
+          'base-100': "#fffef9",                   
+          info: "#0090bc",                  
+          success: "#00fdc8",                 
+          warning: "#ff5300",                  
+          error: "#ff003f",
+          },
+        },
+      ],
     },
-  },
-  plugins: [],
 };
 export default config;
