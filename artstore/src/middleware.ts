96420e4782b5f8d131c4c16b593d052fdee2f0b1
sb989 <sb989@student.le.ac.uk>
2024-11-13 22:56:22 +0000

    Add product route to middleware configuration

--- artstore/src/middleware.ts
+++ artstore/src/middleware.ts
@@ -1,3 +1,3 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/myProfile(/*)?", "/my-products(/.*)?"] }
+export const config = { matcher: ["/myProfile(/*)?", "/my-products(/.*)?", "/product(/.*)?"] }
